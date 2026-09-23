c0ff015ce2301a7354f57474602e5d678013cb36
HOANG KIM SANG <133389686+sengu107@users.noreply.github.com>
2026-09-23 16:42:42 +0700

    Add files via upload

---
+++ Bai 4.x /Đặt Hàng/fastfood.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace dangkyhocvien
+{
+    partial class fastfood
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnRM = new Button();
+            btnADD = new Button();
+            lblTotal = new Label();
+            lstMenu = new ListBox();
+            lstSelected = new ListBox();
+            SuspendLayout();
+            // 
+            // btnRM
+            // 
+            btnRM.Location = new Point(236, 348);
+            btnRM.Name = "btnRM";
+            btnRM.Size = new Size(94, 29);
+            btnRM.TabIndex = 1;
+            btnRM.Text = "<";
+            btnRM.UseVisualStyleBackColor = true;
+            btnRM.Click += btnRemove_Click;
+            // 
+            // btnADD
+            // 
+            btnADD.Location = new Point(348, 348);
+            btnADD.Name = "btnADD";
+            btnADD.Size = new Size(94, 29);
+            btnADD.TabIndex = 2;
+            btnADD.Text = ">";
+            btnADD.UseVisualStyleBackColor = true;
+            btnADD.Click += btnAdd_Click;
+            // 
+            // lblTotal
+            // 
+            lblTotal.AutoSize = true;
+            lblTotal.Location = new Point(236, 293);
+            lblTotal.Name = "lblTotal";
+            lblTotal.Size = new Size(49, 20);
+            lblTotal.TabIndex = 3;
+            lblTotal.Text = "Total :";
+            // 
+            // lstMenu
+            // 
+            lstMenu.FormattingEnabled = true;
+            lstMenu.Location = new Point(92, 77);
+            lstMenu.Name = "lstMenu";
+            lstMenu.Size = new Size(267, 184);
+            lstMenu.TabIndex = 4;
+            // 
+            // lstSelected
+            // 
+            lstSelected.FormattingEnabled = true;
+            lstSelected.Location = new Point(400, 77);
+            lstSelected.Name = "lstSelected";
+            lstSelected.Size = new Size(292, 184);
+            lstSelected.TabIndex = 5;
+            lstSelected.SelectedIndexChanged += listBox2_SelectedIndexChanged;
+            // 
+            // fastfood
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lstSelected);
+            Controls.Add(lstMenu);
+            Controls.Add(lblTotal);
+            Controls.Add(btnADD);
+            Controls.Add(btnRM);
+            Name = "fastfood";
+            Text = "fastfood";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnRM;
+        private Button btnADD;
+        private Label lblTotal;
+        private ListBox lstMenu;
+        private ListBox lstSelected;
+
+  
+
+
+}
+
+}
---
+++ Bai 4.x /Đặt Hàng/fastfood.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace dangkyhocvien
+{
+    public partial class fastfood : Form
+    {
+        Dictionary<string, int> menu = new Dictionary<string, int>()
+        {
+            { "Hamburger", 50000 },
+            { "Pizza", 120000 },
+            { "Gà Rán", 35000 },
+            { "Pepsi", 15000 }
+        };
+
+        public fastfood()
+        {
+            InitializeComponent();
+
+            foreach (var item in menu)
+            {
+                lstMenu.Items.Add(item.Key + " - " + item.Value.ToString("N0") + " VNĐ");
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (lstMenu.SelectedItem != null)
+            {
+                string item = lstMenu.SelectedItem.ToString();
+
+                lstSelected.Items.Add(item);
+                lstMenu.Items.Remove(item);
+
+                UpdateTotal();
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (lstSelected.SelectedItem != null)
+            {
+                string item = lstSelected.SelectedItem.ToString();
+
+                lstSelected.Items.Remove(item);
+                lstMenu.Items.Add(item);
+
+                UpdateTotal();
+            }
+        }
+
+        private void UpdateTotal()
+        {
+            int total = 0;
+
+            foreach (string item in lstSelected.Items)
+            {
+                string name = item.Substring(0, item.LastIndexOf(" - "));
+                total += menu[name];
+            }
+
+            lblTotal.Text = "Total: " + total.ToString("N0") + " VNĐ";
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
