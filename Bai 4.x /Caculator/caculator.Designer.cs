625da8310bd6cef529be351e44060993de4cee51
HOANG KIM SANG <133389686+sengu107@users.noreply.github.com>
2026-09-23 16:15:43 +0700

    Add files via upload

---
+++ Bai 4.x /Caculator/caculator.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace dangkyhocvien
+{
+    partial class caculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btn0 = new Button();
+            btn8 = new Button();
+            btn7 = new Button();
+            btn3 = new Button();
+            btn4 = new Button();
+            btn5 = new Button();
+            btn1 = new Button();
+            btn2 = new Button();
+            btn9 = new Button();
+            btn6 = new Button();
+            btn_nhan = new Button();
+            btn_tru = new Button();
+            btn_cong = new Button();
+            btn_chia = new Button();
+            btn_ketqua = new Button();
+            btn_xoa = new Button();
+            txtDisplay = new TextBox();
+            SuspendLayout();
+            // 
+            // btn0
+            // 
+            btn0.Location = new Point(304, 346);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(44, 43);
+            btn0.TabIndex = 0;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            btn0.Click += NumberButton_Click;
+            // 
+            // btn8
+            // 
+            btn8.Location = new Point(304, 167);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(44, 43);
+            btn8.TabIndex = 1;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += NumberButton_Click;
+            // 
+            // btn7
+            // 
+            btn7.Location = new Point(243, 167);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(44, 43);
+            btn7.TabIndex = 2;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += NumberButton_Click;
+            // 
+            // btn3
+            // 
+            btn3.Location = new Point(367, 288);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(44, 43);
+            btn3.TabIndex = 3;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += NumberButton_Click;
+            // 
+            // btn4
+            // 
+            btn4.Location = new Point(243, 226);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(44, 43);
+            btn4.TabIndex = 4;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += NumberButton_Click;
+            // 
+            // btn5
+            // 
+            btn5.Location = new Point(306, 226);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(44, 43);
+            btn5.TabIndex = 5;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += NumberButton_Click;
+            // 
+            // btn1
+            // 
+            btn1.Location = new Point(243, 288);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(44, 43);
+            btn1.TabIndex = 6;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += NumberButton_Click;
+            // 
+            // btn2
+            // 
+            btn2.Location = new Point(304, 288);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(44, 43);
+            btn2.TabIndex = 7;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += NumberButton_Click;
+            // 
+            // btn9
+            // 
+            btn9.Location = new Point(367, 167);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(44, 43);
+            btn9.TabIndex = 8;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += NumberButton_Click;
+            // 
+            // btn6
+            // 
+            btn6.Location = new Point(367, 226);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(44, 43);
+            btn6.TabIndex = 9;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += NumberButton_Click;
+            // 
+            // btn_nhan
+            // 
+            btn_nhan.Location = new Point(451, 288);
+            btn_nhan.Name = "btn_nhan";
+            btn_nhan.Size = new Size(44, 43);
+            btn_nhan.TabIndex = 10;
+            btn_nhan.Text = "*";
+            btn_nhan.UseVisualStyleBackColor = true;
+            btn_nhan.Click += OperationButton_Click;
+            // 
+            // btn_tru
+            // 
+            btn_tru.Location = new Point(451, 226);
+            btn_tru.Name = "btn_tru";
+            btn_tru.Size = new Size(44, 43);
+            btn_tru.TabIndex = 11;
+            btn_tru.Text = "-";
+            btn_tru.UseVisualStyleBackColor = true;
+            btn_tru.Click += OperationButton_Click;
+            // 
+            // btn_cong
+            // 
+            btn_cong.Location = new Point(451, 167);
+            btn_cong.Name = "btn_cong";
+            btn_cong.Size = new Size(44, 43);
+            btn_cong.TabIndex = 12;
+            btn_cong.Text = "+";
+            btn_cong.UseVisualStyleBackColor = true;
+            btn_cong.Click += OperationButton_Click;
+            // 
+            // btn_chia
+            // 
+            btn_chia.Location = new Point(451, 346);
+            btn_chia.Name = "btn_chia";
+            btn_chia.Size = new Size(44, 43);
+            btn_chia.TabIndex = 13;
+            btn_chia.Text = "/";
+            btn_chia.UseVisualStyleBackColor = true;
+            btn_chia.Click += OperationButton_Click;
+            // 
+            // btn_ketqua
+            // 
+            btn_ketqua.Location = new Point(367, 346);
+            btn_ketqua.Name = "btn_ketqua";
+            btn_ketqua.Size = new Size(44, 43);
+            btn_ketqua.TabIndex = 14;
+            btn_ketqua.Text = "=";
+            btn_ketqua.UseVisualStyleBackColor = true;
+            btn_ketqua.Click += btn_ketqua_Click;
+            // 
+            // btn_xoa
+            // 
+            btn_xoa.Location = new Point(243, 346);
+            btn_xoa.Name = "btn_xoa";
+            btn_xoa.Size = new Size(44, 43);
+            btn_xoa.TabIndex = 15;
+            btn_xoa.Text = "C";
+            btn_xoa.UseVisualStyleBackColor = true;
+            btn_xoa.Click += btn_xoa_Click;
+            // 
+            // txtDisplay
+            // 
+            txtDisplay.Location = new Point(207, 96);
+            txtDisplay.Name = "txtDisplay";
+            txtDisplay.Size = new Size(317, 27);
+            txtDisplay.TabIndex = 17;
+            // 
+            // caculator
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtDisplay);
+            Controls.Add(btn_xoa);
+            Controls.Add(btn_ketqua);
+            Controls.Add(btn_chia);
+            Controls.Add(btn_cong);
+            Controls.Add(btn_tru);
+            Controls.Add(btn_nhan);
+            Controls.Add(btn6);
+            Controls.Add(btn9);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Controls.Add(btn5);
+            Controls.Add(btn4);
+            Controls.Add(btn3);
+            Controls.Add(btn7);
+            Controls.Add(btn8);
+            Controls.Add(btn0);
+            Name = "caculator";
+            Text = "caculator";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btn0;
+        private Button btn8;
+        private Button btn7;
+        private Button btn3;
+        private Button btn4;
+        private Button btn5;
+        private Button btn1;
+        private Button btn2;
+        private Button btn9;
+        private Button btn6;
+        private Button btn_nhan;
+        private Button btn_tru;
+        private Button btn_cong;
+        private Button btn_chia;
+        private Button btn_ketqua;
+        private Button btn_xoa;
+        private TextBox txtDisplay;
+
+    }
+}
---
+++ Bai 4.x /Caculator/caculator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace dangkyhocvien
+{
+    public partial class caculator : Form
+    {
+        public caculator()
+        {
+            InitializeComponent();
+        }
+        double result = 0;
+        string operation = "";
+        private void NumberButton_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            txtDisplay.Text += button.Text;
+        }
+        private void OperationButton_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            operation = button.Text;
+            result = double.Parse(txtDisplay.Text);
+            txtDisplay.Clear();
+        }
+        private void btn_ketqua_Click(object sender, EventArgs e)
+        {
+            double secondNumber = double.Parse(txtDisplay.Text);
+            switch (operation)
+            {
+                case "+":
+                    result += secondNumber;
+                    break;
+                case "-":
+                    result -= secondNumber;
+                    break;
+                case "*":
+                    result *= secondNumber;
+                    break;
+                case "/":
+                    if (secondNumber != 0)
+                        result /= secondNumber;
+                    else
+                        MessageBox.Show("Cannot divide by zero");
+                    break;
+            }
+            txtDisplay.Text = result.ToString();
+        }
+        private void btn_xoa_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Clear();
+            result = 0;
+            operation = "";
+        }
+    }
+}
