9b9597e629e6d818277bf181b6fafe0fe274e7c4
HOANG KIM SANG <133389686+sengu107@users.noreply.github.com>
2026-09-23 14:52:56 +0700

    Add files via upload

---
+++ Bai 4.x /Login Form/Login.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace WinFormsApp1
+{
+    partial class Login
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            txtPassword = new TextBox();
+            txtUsername = new TextBox();
+            btnExit = new Button();
+            label1 = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            label3 = new Label();
+            label2 = new Label();
+            btnLogin = new Button();
+            chkShowPassword = new CheckBox();
+            errorProvider1 = new ErrorProvider(components);
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            SuspendLayout();
+            // 
+            // txtPassword
+            // 
+            txtPassword.Dock = DockStyle.Fill;
+            txtPassword.Location = new Point(193, 139);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(374, 27);
+            txtPassword.TabIndex = 0;
+            txtPassword.UseSystemPasswordChar = true;
+            txtPassword.TextChanged += textBox1_TextChanged;
+            // 
+            // txtUsername
+            // 
+            txtUsername.AllowDrop = true;
+            txtUsername.Dock = DockStyle.Fill;
+            txtUsername.Location = new Point(193, 71);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(374, 27);
+            txtUsername.TabIndex = 1;
+            // 
+            // btnExit
+            // 
+            btnExit.Anchor = AnchorStyles.None;
+            btnExit.Location = new Point(333, 291);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(94, 29);
+            btnExit.TabIndex = 2;
+            btnExit.Text = "Exit";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14F);
+            label1.Location = new Point(193, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(374, 68);
+            label1.TabIndex = 3;
+            label1.Text = "Đăng nhập";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label1.Click += label1_Click;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 25F));
+            tableLayoutPanel1.Controls.Add(txtPassword, 1, 2);
+            tableLayoutPanel1.Controls.Add(label1, 1, 0);
+            tableLayoutPanel1.Controls.Add(txtUsername, 1, 1);
+            tableLayoutPanel1.Controls.Add(label3, 0, 2);
+            tableLayoutPanel1.Controls.Add(label2, 0, 1);
+            tableLayoutPanel1.Controls.Add(btnLogin, 1, 3);
+            tableLayoutPanel1.Controls.Add(btnExit, 1, 4);
+            tableLayoutPanel1.Controls.Add(chkShowPassword, 2, 2);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 5;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 20F));
+            tableLayoutPanel1.Size = new Size(761, 340);
+            tableLayoutPanel1.TabIndex = 4;
+            tableLayoutPanel1.Paint += tableLayoutPanel1_Paint;
+            // 
+            // label3
+            // 
+            label3.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label3.AutoSize = true;
+            label3.Location = new Point(3, 136);
+            label3.Name = "label3";
+            label3.Size = new Size(184, 68);
+            label3.TabIndex = 5;
+            label3.Text = "Password";
+            label3.TextAlign = ContentAlignment.TopRight;
+            label3.Click += label3_Click;
+            // 
+            // label2
+            // 
+            label2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Location = new Point(69, 68);
+            label2.Name = "label2";
+            label2.Size = new Size(118, 20);
+            label2.TabIndex = 4;
+            label2.Text = "Username/Email";
+            label2.TextAlign = ContentAlignment.TopRight;
+            label2.Click += label2_Click;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Anchor = AnchorStyles.None;
+            btnLogin.Location = new Point(333, 223);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(94, 29);
+            btnLogin.TabIndex = 6;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            btnLogin.Click += btn_Login_CLick;
+            // 
+            // chkShowPassword
+            // 
+            chkShowPassword.AutoSize = true;
+            chkShowPassword.Location = new Point(573, 139);
+            chkShowPassword.Name = "chkShowPassword";
+            chkShowPassword.Size = new Size(132, 24);
+            chkShowPassword.TabIndex = 7;
+            chkShowPassword.Text = "Show Password";
+            chkShowPassword.UseVisualStyleBackColor = true;
+            chkShowPassword.CheckedChanged += chkShowPassword_CheckedChanged;
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // Login
+            // 
+            AcceptButton = btnLogin;
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            CancelButton = btnExit;
+            ClientSize = new Size(761, 340);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Login";
+            Text = "Login";
+            Load += Form1_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TextBox txtPassword;
+        private TextBox txtUsername;
+        private Button btnExit;
+        private Label label1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private Label label2;
+        private Label label3;
+        private Button btnLogin;
+        private void btn_Login_CLick(object sender, EventArgs e)
+        {
+            bool isValid = true;
+            errorProvider1.Clear();
+            if (string.IsNullOrWhiteSpace(txtUsername.Text))
+            {
+                errorProvider1.SetError(txtUsername, "Username/Email không được để trống.");
+                isValid = false;
+            }
+            if (string.IsNullOrWhiteSpace(txtPassword.Text))
+            {
+                errorProvider1.SetError(txtPassword, "Password không được để trống.");
+                isValid = false;
+            }
+            if (isValid)
+            {
+                MessageBox.Show("Đăng nhập thành công!");
+            }
+
+        }
+        private void chkShowPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            txtPassword.UseSystemPasswordChar = !chkShowPassword.Checked;
+        }
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private CheckBox chkShowPassword;
+        private ErrorProvider errorProvider1;
+    }
+}
---
+++ Bai 4.x /Login Form/Login.cs
@@ -0,0 +1,40 @@
+namespace WinFormsApp1
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
